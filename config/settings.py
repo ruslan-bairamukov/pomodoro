941b95e07462d463ef24d866e4c53911822a297c
Ruslan Bairamukov <rusbay.ts@gmail.com>
2025-01-03 16:17:12 +0300

    Add sqlite3 db to settings

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     GOOGLE_TOKEN_ID: str = "2803463467272300832"
+    sqlite_db_name: str = "pomodoro.db"
 
     model_config = SettingsConfigDict(env_file=".dev.env")
 
