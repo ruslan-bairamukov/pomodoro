e6f51f258ed6f7478b26eb5221c52c52eb037eaa
Ruslan Bairamukov <rusbay.ts@gmail.com>
2025-01-05 13:25:59 +0300

    Create engine

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
 
 from config.settings import settings
 
 
-def get_db_connection() -> sqlite3.Connection:
-    return sqlite3.connect(settings.sqlite_db_name)
+engine = create_engine("sqlite:///pomodoro.db")
+
+
+def get_db_connection() -> Session:
+    return sessionmaker(engine)
