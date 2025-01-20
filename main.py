e7327af7ebd2e6d1350cf9b3081ced1539c0ea8c
ruslan-bairamukov <rusbay.ts@gmail.com>
2025-01-20 16:48:23 +0300

    Add comment tp main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 
 from handlers import routers
 
-
 app = FastAPI()
 for router in routers:
     app.include_router(router=router)
+
+
+# provide for better branch history
